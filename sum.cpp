bc14fd6162b3e218458edfcc50b7342333176d2e
Shashi Kumar <shashisaurav50@gmail.com>
2022-06-30 19:43:56 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+typedef pair<int, int> pii;
+typedef pair<int, pair<int, int>> piii;
+typedef pair<int, char> pic;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+ 
+
+ 
+int main (){
+std::ios_base::sync_with_stdio(false);
+
+int a = 5;
+int b = 2;
+int sum = a + b;
+cout<<sum<<endl;
+return 0;
+}
